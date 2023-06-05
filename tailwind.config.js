38ffb23a8994c2367d6a9a580ec6e03b5a90e771
enzom-uy <enzom.dev@gmail.com>
2023-06-05 18:44:58 -0300

    Customize tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-csgo":
+          "linear-gradient(90deg, #5D79AE 45.84%, rgba(204, 186, 124, 0.8) 85.57%, #DE9B35 95.28%)",
+      },
+      colors: {
+        dark: "#030711",
+        "dark-secondary": "#0f172a",
+        light: "#e2e8f0",
+        "light-muted": "#7f8e92",
+        "accent-dark": "#38bdf8",
+        "border-dark": "#1d283a",
       },
     },
   },
   plugins: [],
-}
+};
