d99e2982cf87d070ef4a186cc3dbb0729232102a
enzom-uy <enzom.dev@gmail.com>
2023-06-08 11:58:10 -0300

    Add responsive padding for Header and transition-all 300ms

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Header = () => {
     return (
         <header className="border-b border-b-border-dark">
-            <nav className="px-3 py-[0.4rem]">
+            <nav className="px-3 py-[0.4rem] md:px-20 lg:px-40 2xl:px-72">
                 <Link
                     href="/"
                     className="text-lg font-semibold text-accent-dark"
