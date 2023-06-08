b36cc95ceecd9b6622160758d831f1d3d0e364b0
enzom-uy <enzom.dev@gmail.com>
2023-06-08 11:49:06 -0300

    Install shadcn-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,86 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-csgo":
-          "linear-gradient(90deg, #5D79AE 45.84%, rgba(204, 186, 124, 0.8) 85.57%, #DE9B35 95.28%)",
-      },
-      colors: {
-        dark: "#030711",
-        "dark-secondary": "#0f172a",
-        light: "#e2e8f0",
-        "light-muted": "#7f8e92",
-        "accent-dark": "#38bdf8",
-        "border-dark": "#1d283a",
-      },
+    darkMode: ['class'],
+    content: [
+        './src/pages/**/*.{ts,tsx}',
+        './src/components/**/*.{ts,tsx}',
+        './src/app/**/*.{ts,tsx}',
+        './src/shad-components/**/*.{ts,tsx}'
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px'
+            }
+        },
+        extend: {
+            backgroundImage: {
+                'gradient-csgo':
+                    'linear-gradient(90deg, #5D79AE 45.84%, rgba(204, 186, 124, 0.8) 85.57%, #DE9B35 95.28%)'
+            },
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                },
+                dark: '#030711',
+                'dark-secondary': '#0f172a',
+                light: '#e2e8f0',
+                'light-muted': '#7f8e92',
+                'accent-dark': '#38bdf8',
+                'border-dark': '#1d283a'
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-accordion-content-height)' }
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: 0 }
+                }
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out'
+            }
+        }
     },
-  },
-  plugins: [],
-};
+    plugins: [require('tailwindcss-animate')]
+}
