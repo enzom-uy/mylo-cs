30acac83bea408c39b8a8586dac63679379b0b64
enzom-uy <enzom.dev@gmail.com>
2023-06-08 12:50:33 -0300

    SearchSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next'
-import Hero from './components/hero'
+import Hero from './components/sections/hero-section'
+import SearchSection from './components/sections/search-section'
 
 const Home: NextPage = () => {
     return (
         <>
             <Hero />
+            <SearchSection />
         </>
     )
 }
