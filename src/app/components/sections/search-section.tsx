4fabf6da3dda75e046e8c4ad60b7b1b7bb0370ea
enzom-uy <enzom.dev@gmail.com>
2023-06-09 11:35:07 -0300

    Add nade author name to search options

--- src/app/components/sections/search-section.tsx
+++ src/app/components/sections/search-section.tsx
@@ -9,7 +9,7 @@ interface Props {
     query?: string
 }
 
-const SearchSection: any = async ({ query }: Props) => {
+const SearchSection = async ({ query }: Props) => {
     if (!query) {
         return (
             <section className="flex w-full flex-col gap-6 pt-6">
@@ -29,14 +29,14 @@ const SearchSection: any = async ({ query }: Props) => {
                 or(
                     like(nade.title, queryWithSpaces),
                     like(nade.mapId, queryWithSpaces),
-                    like(nade.nadeTypeName, queryWithSpaces)
+                    like(nade.nadeTypeName, queryWithSpaces),
+                    like(nade.authorId, queryWithSpaces)
                 )
             )
         return nades
     }
     const foundedNades = await getNades()
 
-    console.log('Rerender')
     return (
         <section className="flex w-full flex-col gap-6 pt-6">
             <SearchInput />
