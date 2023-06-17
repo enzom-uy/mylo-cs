858955b4ef75944cc134bff5b92ce3bc6b288146
enzom-uy <enzom.dev@gmail.com>
2023-06-17 13:11:17 -0300

    Install prettier plugin prisma

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,8 @@ module.exports = {
     tabWidth: 4,
     semi: false,
     singleQuote: true,
-    plugins: [require('prettier-plugin-tailwindcss')]
+    plugins: [
+        require('prettier-plugin-tailwindcss'),
+        require('prettier-plugin-prisma')
+    ]
 }
