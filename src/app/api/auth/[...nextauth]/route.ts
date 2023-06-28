06f878af2fd278dfbbdc5373a0bb29b2c3ceb0b4
enzom-uy <enzom.dev@gmail.com>
2023-06-28 14:04:55 -0300

    Add user to servers he's already in if the server exist in the DB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from '@/config/db'
 import { Adapter } from 'next-auth/adapters'
 import { AuthOptions } from 'next-auth'
+import axios from 'axios'
+import { Guild } from '@/app/create-server/utils/getUserGuilds'
 
 let userId: string
 let newUser: boolean
@@ -76,6 +78,50 @@ export const authOptions: AuthOptions = {
                     providerAccountId: userId
                 }
             })
+
+            const userGuilds = await axios
+                .get('https://discordapp.com/api/users/@me/guilds', {
+                    headers: {
+                        Authorization: 'Bearer ' + userAccount?.access_token
+                    }
+                })
+                .then((res) => res.data as Guild[])
+
+            const guildsId = userGuilds.map((g) => ({ id: { contains: g.id } }))
+
+            const userIsInExistingServers = await db.server.findMany({
+                where: {
+                    OR: guildsId
+                }
+            })
+            console.log('User is in existing servers:', userIsInExistingServers)
+            if (!!userIsInExistingServers) {
+                userIsInExistingServers.forEach(
+                    async (g) =>
+                        await db.user.update({
+                            where: {
+                                id: userAccount?.providerAccountId
+                            },
+                            data: {
+                                servers_is_member: {
+                                    connect: {
+                                        id: g.id
+                                    }
+                                },
+                                user_server_role: {
+                                    create: {
+                                        role: 'USER',
+                                        server: {
+                                            connect: {
+                                                id: g.id
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                )
+            }
         }
     }
 }
