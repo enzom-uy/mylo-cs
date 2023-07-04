1f35a288f139827ae40ca003874ffc13d57d0a64
enzom-uy <enzom.dev@gmail.com>
2023-07-04 19:34:19 -0300

    Database dev url

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const db =
     globalForPrisma.prisma ||
     new PrismaClient({
-        log: ['query', 'info', 'warn', 'error']
+        log: ['info', 'warn', 'error']
     })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
