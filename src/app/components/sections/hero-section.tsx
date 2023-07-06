8f39e170e67499e8dc4effb671b0f3b14f5938cb
enzom-uy <enzom.dev@gmail.com>
2023-07-06 18:54:06 -0300

    hero spacing and content

--- src/app/components/sections/hero-section.tsx
+++ src/app/components/sections/hero-section.tsx
@@ -7,8 +7,8 @@ const Hero = async () => {
     const session = await getServerSession(authOptions)
     return (
         <section className="flex w-full justify-center border-b border-b-border-dark px-10 pb-6">
-            <div className="flex flex-col gap-3">
-                <h1 className="text-3xl font-semibold">
+            <div className="flex flex-col gap-4">
+                <h1 className="m-0 text-3xl font-semibold">
                     Mejora tu juego con granadas útiles.
                 </h1>
                 <div className="max-w-md font-semibold text-light-muted">
@@ -17,8 +17,11 @@ const Hero = async () => {
                         <span className="csgo"> Counter-Strike</span>.
                     </p>
                     <p>
-                        Accede a nuestra colección de granadas y lleva tu juego
-                        al siguiente nivel.
+                        Lleva tu juego al siguiente nivel. Une{' '}
+                        <span className="text-[#7289DA]">Discord</span> y
+                        <span className="text-accent-dark"> mylo</span>
+                        <span className="csgo">/csgo</span> para crear, ver y
+                        compartir las granadas más útiles.
                     </p>
                 </div>
                 <div className="flex flex-wrap items-center gap-4">
