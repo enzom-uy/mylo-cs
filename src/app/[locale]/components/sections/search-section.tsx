2a16f15d026aa062a3e4d1c86879edef9f7aa5ac
enzom-uy <enzom.dev@gmail.com>
2023-08-07 04:24:29 -0300

    search section

--- src/app/[locale]/components/sections/search-section.tsx
+++ src/app/[locale]/components/sections/search-section.tsx
@@ -2,25 +2,51 @@ import { getNadesQuery } from '@/services/getNadesQuery'
 import { useTranslations } from 'next-intl'
 import SearchInput from '../search-input'
 import NadesSection from './nades-section'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 interface Props {
     query?: string
-    userId: string
 }
 
-const Msg = () => {
+const SearchMsg = () => {
     const t = useTranslations()
     return <p>{t('Search-Section.search-msg')}</p>
 }
 
-const SearchSection = async ({ query, userId }: Props) => {
-    const foundedNades = await getNadesQuery({ query, userId })
+const MustLoginMsg = () => {
+    const t = useTranslations()
+    return <p className="mt-6">{t('Search-Section.must-login')}</p>
+}
+
+const SearchSection = async ({ query }: Props) => {
+    const session = await getServerSession(authOptions)
+    const foundedNades = await getNadesQuery({
+        query,
+        userId: session?.id as string
+    })
+    console.log(session && !!!query)
 
     return (
         <section className="flex w-full flex-col items-center gap-6 pt-6">
-            <SearchInput />
-            {!query && <Msg />}
-            {query && <NadesSection nades={foundedNades} userId={userId} />}
+            {session && query ? (
+                <SearchInput />
+            ) : !session ? (
+                <MustLoginMsg />
+            ) : (
+                session &&
+                !!!query && (
+                    <>
+                        <SearchInput /> <SearchMsg />
+                    </>
+                )
+            )}
+            {query && (
+                <NadesSection
+                    nades={foundedNades}
+                    userId={session?.id as string}
+                />
+            )}
         </section>
     )
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,28 +4,17 @@ import { authOptions } from '../api/auth/[...nextauth]/route'
 import Hero from './components/sections/hero-section'
 import SearchSection from './components/sections/search-section'
 
-const Msg = () => {
-    const t = useTranslations()
-    return <p className="mt-6">{t('Search-Section.must-login')}</p>
-}
-
 const Home = async ({
     searchParams
 }: {
     searchParams: { [key: string]: string | string[] | undefined }
 }) => {
-    const session = await getServerSession(authOptions)
     return (
         <>
             <Hero />
-            {session ? (
-                <SearchSection
-                    query={searchParams.query && (searchParams.query as string)}
-                    userId={session?.id}
-                />
-            ) : (
-                <Msg />
-            )}
+            <SearchSection
+                query={searchParams.query && (searchParams.query as string)}
+            />
         </>
     )
 }
